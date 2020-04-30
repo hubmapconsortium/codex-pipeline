82a556754e9be1275b828524b1da208e4efc4b88
Matt Ruffalo <matt.ruffalo@gmail.com>
2020-04-30 19:17:20 -0400

    Remove duplicate functions
    After seeing another copy of 'infer_tile_names', I kept thinking "I thought
    I tweaked this already", and thought I was going crazy until finding that
    I'd modified the other copy. No reason to have two.

--- bin/create_visualization_dir.py
+++ bin/create_visualization_dir.py
@@ -10,7 +10,7 @@ import tarfile
 from typing import Dict, List, Tuple
 import yaml
 
-from utils import list_directory_tree
+from utils import collect_files_by_tile, infer_tile_names, list_directory_tree
 
 import os
 
@@ -19,54 +19,6 @@ logging.basicConfig(
     format='%(levelname)-7s - %(message)s',
 )
 logger = logging.getLogger(__name__)
-
-
-def infer_tile_names( cytokit_config_filename: Path ) -> List[str] :
-
-    with open( cytokit_config_filename, 'r' ) as cytokit_config_file:
-        cytokit_config = yaml.safe_load( cytokit_config_file )
-
-    tile_names = []
-
-    region_height, region_width = (
-        cytokit_config[ "acquisition" ][ "region_height" ],
-        cytokit_config[ "acquisition" ][ "region_width" ],
-    )
-    region_names = cytokit_config[ "acquisition" ][ "region_names" ]
-
-    for r in range( 1, len( region_names ) + 1 ) :
-        # Width is X values, height is Y values.
-        for x in range( 1, region_width + 1 ) :
-            for y in range( 1, region_height + 1 ) :
-                tile_names.append( f"R{r:03}_X{x:03}_Y{y:03}" )
-
-    return tile_names
-
-
-def collect_files_by_tile(
-    tile_names: List[str],
-    directory: Path,
-) -> Dict[str, List[Path]] :
-
-    files_by_tile: Dict[str, List[Path]] = defaultdict(list)
-
-    for tile in tile_names :
-        tile_name_pattern = re.compile( tile )
-
-        for dirpath_str, dirnames, filenames in walk( directory ) :
-            dirpath = Path(dirpath_str)
-            for filename in filenames :
-                if tile_name_pattern.match( filename ) :
-                    files_by_tile[ tile ].append( dirpath / filename )
-
-    # If a tile doesn't have any files, throw an error.
-    for tile in tile_names :
-        if len( files_by_tile[ tile ] ) == 0 :
-            raise ValueError(
-                f"No files were found for tile {tile}"
-            )
-
-    return files_by_tile
 
 def create_relative_symlink_target(
         input_file_path: Path,
