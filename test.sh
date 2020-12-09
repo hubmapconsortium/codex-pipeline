581798d7fb345545646cac42174257c86cc71a96
Matt Ruffalo <matt.ruffalo@gmail.com>
2020-12-09 11:14:00 -0500

    Add barebones Travis config

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+
+start() { echo travis_fold':'start:$1; echo $1; }
+end() { set +v; echo travis_fold':'end:$1; echo; echo; }
+die() { set +v; echo "$*" 1>&2 ; exit 1; }
+
+start black
+black --check .
+end black
