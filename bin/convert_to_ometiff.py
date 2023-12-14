72c83193dd2d608f49050e1d901c0350bb246755
pennycuda <pcuda@andrew.cmu.edu>
2023-12-14 11:15:30 -0500

    Do not run as a series

--- bin/convert_to_ometiff.py
+++ bin/convert_to_ometiff.py
@@ -200,13 +200,13 @@ def create_ome_tiffs(
             )
         )
     # Uncomment the next line to run as a series, comment the plural line
-    for argtuple in args_for_conversion:
-        convert_tiff_file(argtuple)
+    # for argtuple in args_for_conversion:
+    #     convert_tiff_file(argtuple)
 
-    # with Pool(processes=subprocesses) as pool:
-    #     pool.imap_unordered(convert_tiff_file, args_for_conversion)
-    #     pool.close()
-    #     pool.join()
+    with Pool(processes=subprocesses) as pool:
+        pool.imap_unordered(convert_tiff_file, args_for_conversion)
+        pool.close()
+        pool.join()
 
 
 def check_dir_is_empty(dir_path: Path):
